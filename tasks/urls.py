3d999609f3ebe72df4cd9b985ac32a2039e99d8e
José Hildebrando <jose.hildebrando@gmail.com>
2020-07-30 09:58:18 -0300

    delete task

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('task/<int:id>',views.taskView,name="task-view"),
     path('newtask/',views.newTask, name="new-task"),
     path('edit/<int:id>',views.editTask,name="edit-task"),
+    path('delete/<int:id>',views.deleteTask, name="deletetask"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Task
 from .forms import TaskForm, TaskFormEdit
 
+
 def taskList(request):
     tasks = Task.objects.all().order_by('-created_at')
     return render(request, 'tasks/list.html',{'tasks':tasks})
 
+
 def taskView(request,id):
     task = get_object_or_404(Task,pk=id)
     return render(request, 'tasks/task.html',{'task':task})
+
 
 def newTask(request):
     if request.method == 'POST':
@@ -22,6 +25,7 @@ def newTask(request):
     else:
         form = TaskForm()
         return render(request, 'tasks/addtask.html',{'form':form})
+
 
 def editTask(request, id):
     task = get_object_or_404(Task, pk=id)
@@ -37,3 +41,11 @@ def editTask(request, id):
 
     else:
         return render(request, 'tasks/edittask.html', {'task':task, 'form':form})
+
+
+def deleteTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+    return redirect('/')
+
+
