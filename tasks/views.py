439bb7c908b6ff00ae47aac454228f80504f633d
José Hildebrando <jose.hildebrando@gmail.com>
2020-08-06 14:49:13 -0300

    paginator

--- tasks/views.py
+++ tasks/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Task
 from .forms import TaskForm, TaskFormEdit
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 
 def taskList(request):
-    tasks = Task.objects.all().order_by('-created_at')
+    tasks_list = Task.objects.all().order_by('-created_at')
+
+    paginator = Paginator(tasks_list, 3)
+    page = request.GET.get('page')
+    tasks = paginator.get_page(page)
+
     return render(request, 'tasks/list.html',{'tasks':tasks})
 
 
