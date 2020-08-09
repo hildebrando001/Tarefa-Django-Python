c08f4d7d6922af6f57811aa25fc9948f8b53216a
José Hildebrando <jose.hildebrando@gmail.com>
2020-08-09 20:35:43 -0300

    Model updated

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Task(models.Model):
 
@@ -11,6 +12,7 @@ class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     done = models.CharField(max_length=7, choices=STATUS)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
