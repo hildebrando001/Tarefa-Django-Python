eb287862499758aff5818187cb5de7c883dbb054
José Hildebrando <jose.hildebrando@gmail.com>
2020-08-10 10:57:02 -0300

    tamplate base updated

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy # same as redirect, but to be used with basedview class
-from django.views import generic # alow as to create a basedview class
+from django.views import generic # alow to create a basedview class
 
 class SignUp(generic.CreateView): # NOT a def  # basedview class
     form_class = UserCreationForm
