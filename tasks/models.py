1171fa2b0b8383d867c89920bab41c430d30c1b4
José Hildebrando <jose.hildebrando@gmail.com>
2020-08-12 21:06:11 -0300

    model updated

--- tasks/models.py
+++ tasks/models.py
@@ -9,12 +9,13 @@ class Task(models.Model):
         ('feito', 'feito'),
     )
 
-    title = models.CharField(max_length=255)
-    description = models.TextField()
+    title = models.CharField('título', max_length=255)
+    description = models.TextField('descrição')
     done = models.CharField(max_length=7, choices=STATUS)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField('data de criação', auto_now_add=True)
+    updated_at = models.DateTimeField('data de atualização', auto_now=True)
+
 
     def __str__(self):
         return self.title
